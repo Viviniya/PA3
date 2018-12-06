a5bcdb6e5bd8f796af1267a6aaeea682365c55b3
vivin <vivinya25@gmail.com>
2018-12-06 01:09:20 -0500

    c

--- csc501-lab3/sys/resched.c
+++ csc501-lab3/sys/resched.c
@@ -20,13 +20,13 @@ int resched()
 	register struct	pentry	*optr;	/* pointer to old process entry */
 	register struct	pentry	*nptr;	/* pointer to new process entry */
 
-	/* no switch needed if current process priority higher than next*/
-
+	/* no switch needed if current process priority higher than any pid(pinh or pprio) in queue*/
+	int pid= return_max_pinh();
 	if ( ( (optr= &proctab[currpid])->pstate == PRCURR) &&
-	   (lastkey(rdytail)<optr->pprio)) {
+	   (pid == currpid) {
 		return(OK);
 	}
-	
+
 	/* force context switch */
 
 	if (optr->pstate == PRCURR) {
@@ -36,14 +36,45 @@ int resched()
 
 	/* remove highest priority process at end of ready list */
 
-	nptr = &proctab[ (currpid = getlast(rdytail)) ];
+	nptr = &proctab[pid];
 	nptr->pstate = PRCURR;		/* mark it currently running	*/
 #ifdef	RTCLOCK
 	preempt = QUANTUM;		/* reset preemption counter	*/
 #endif
-	
+
 	ctxsw((int)&optr->pesp, (int)optr->pirmask, (int)&nptr->pesp, (int)nptr->pirmask);
-	
+
 	/* The OLD process returns here when resumed. */
 	return OK;
+}
+int return_max_pinh(){
+	int	next;			/* runs through list		*/
+	int	prev;
+	next = q[rdyhead].qnext;
+	int flag =1;
+	int curprio=-1;
+	if(proctab[currpid].pinh!= 0)
+		curprio=proctab[currpid].pinh;
+	else
+		curprio=proctab[currpid].pprio;
+		int pid = currpid;
+	while (q[next])	{
+		 kprintf("output pid=%d\n", next);
+		 if(proctab[next].pinh!=0){
+			  if(curprio < proctab[next].pinh){
+					pid=next;
+					curprio=proctab[next].pinh;
+				}
+
+		 }
+		 else{
+			 if(curprio < proctab[next].pprio){
+				  pid=next;
+						curprio=proctab[next].pprio;
+			 }
+
+		 }
+			next = q[next].qnext;
+	}
+	return pid;
 }
