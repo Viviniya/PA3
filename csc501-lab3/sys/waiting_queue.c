b528aefd13ddb037a722a8dd6514c2d0650e2ff3
vivin <vivinya25@gmail.com>
2018-12-06 01:09:34 -0500

    c

--- csc501-lab3/sys/waiting_queue.c
+++ csc501-lab3/sys/waiting_queue.c
@@ -3,16 +3,9 @@
 #include <proc.h>
 #include <lock.h>
 
-extern struct lqueue *head_cirQ;
-extern struct lqueue *tail_cirQ;
 
-void init() {
-    STATWORD ps;
-    disable(ps);
-    head_cirQ= NULL;
-    tail_cirQ = NULL;
-    restore(ps);
-}
+
+
 void init_data(lqueue *framed,int ldes1, int  type,  int priority) {
     STATWORD ps;
     disable(ps);
@@ -27,15 +20,15 @@ void init_data(lqueue *framed,int ldes1, int  type,  int priority) {
     restore(ps);
 }
 
-void enqueue_c(int ldes1, int  type,  int priority) {
+void enqueue_c(int ldes1, int  type,  int priority,struct lqueue *head_cirQ, struct lqueue *tail_cirQ) {
     STATWORD ps;
     disable(ps);
     int sizeQueue=sizeof (struct lqueue);
     struct lqueue *framed = (struct lqueue*) getmem(sizeQueue);
     init_data(&framed, ldes1,   type,   priority);
-    if (head_cirQ == NULL && tail_cirQ == NULL) {
- kprintf("Enqueued first call: %d \n",ldes1 );
-        head_cirQ = tail_cirQ= framed;
+    if (head_cirQ == NULL ) {
+    kprintf("Enqueued first call: %d \n",ldes1 );
+        head_cirQ = *tail_cirQ= framed;
     } else {
         tail_cirQ->next = framed;
         tail_cirQ = framed;
@@ -46,54 +39,18 @@ void enqueue_c(int ldes1, int  type,  int priority) {
     restore(ps);
 }
 
-int delete_frame(lqueue *previous, int id) {
-    STATWORD ps;
-    disable(ps);
-    int sizeQueue=sizeof (struct lqueue);
-    struct lqueue *current = (struct lqueue*) getmem(sizeQueue);
-
-    if (head_cirQ != NULL) {
-	kprintf("delete_frame: %d \n",id );
-      if(previous ==NULL){ // if the head is to be removed
-        current= head_cirQ;
-        if(current->frametab_id == id){
-          head_cirQ= head_cirQ->next;
-          freemem(current, sizeQueue);
-        }
-      }
-      else{
-            current = previous->next;
-          if(current->frametab_id == id){
-            previous->next=current->next;
-            freemem(current, sizeQueue);
-          }
-          else{
-          //  freemem(current, sizeQueue);
-            restore(ps);
-            return SYSERR;
-          }
-
-
-      }
-      restore(ps);
-      return OK;
-    } else {
-        restore(ps);
-        return SYSERR;
-    }
-}
-void delete_fromQ_withID(int pid){
+void delete_fromQ_withID(int pid, struct lqueue *head_cirQ){
   int sizeQueue=sizeof (struct lqueue);
   int loop=1;
-  if (head_cirQ == NULL) {
+  if (*head_cirQ == NULL) {
     return SYSERR;
   }
   struct lqueue *current = (struct lqueue*) getmem(sizeQueue);
   struct lqueue *previous = (struct lqueue*) getmem(sizeQueue);
   while(loop !=0){
 	kprintf("delete_deomQ: %d \n",pid );
-      current= head_cirQ;
-      if(current->frametab_id == pid){
+      current= *head_cirQ;
+      if(framed->pid  == pid){
           if(previous == NULL){
           head_cirQ= head_cirQ->next;
           freemem(current, sizeQueue);
@@ -114,4 +71,5 @@ void delete_fromQ_withID(int pid){
     current=current->next;
 
   }
+  return SYSERR;
 }
