ccacf7414c68f6152b00b2b91ea9b7b834d4097a
vivin <vivinya25@gmail.com>
2018-12-06 01:08:06 -0500

    commit git commit chprio.c -m commit

--- csc501-lab3/sys/chprio.c
+++ csc501-lab3/sys/chprio.c
@@ -22,10 +22,32 @@ SYSCALL chprio(int pid, int newprio)
 		return(SYSERR);
 	}
 	pptr->pprio = newprio;
-	if(pptr -> pstate == PRREADY) {
-		dequeue(pid);
-		insert(pid, rdyhead, pptr -> pprio);
+	switch (pptr->pstate) {
+	case PRREADY:
+		insert( dequeue(pid), rdyhead, newprio);
+	case PRCURR:
+		resched();
+	default:
+		break;
 	}
+	if(proctab[pid].waiting_in_lock != -1){ // if the process is currently in waitlist of a lock
+		int lock = proctab[pid].waiting_in_lock;
+		inherit_changed_priority( lock,  newprio);
+	}
+
+	// need to inherit priority
 	restore(ps);
 	return(newprio);
+}
+void inherit_changed_priority(int ldes1, int newprio){
+  int i;
+  int maxp=newprio;
+  for(i=0;i<NPROC;i++){
+    if(  locks_table[ldes1].process_locked[i] == LOCKED){
+        if(maxp>proctab[i].pprio){
+            proctab[i].pinh=maxp;
+            locks_table[ldes1].max_process_priority=maxp;
+        }
+    }
+  }
 }
